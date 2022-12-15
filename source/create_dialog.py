4a51707b086e233a88bf4e4fb24830718735fd20
xtern0o <dos30028gmail.com>
2022-12-16 01:53:10 +0300

    added shortcuts

---
+++ source/create_dialog.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeyEvent
+
+from source.ui_generated_py_files.ui_create_dialog import Ui_Dialog
+
+
+class CreateDialog(Ui_Dialog, QDialog):
+    def __init__(self, user_id, parent=None):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+
+        self.main_menu = parent
+        self.user_id = user_id
+
+        self.stackedWidget.setCurrentIndex(0)
+
+        self.add_payment_btn.clicked.connect(self.goto_new_payment)
+        self.add_category_btn.clicked.connect(self.goto_new_category)
+
+    def goto_new_payment(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def goto_new_category(self):
+        self.stackedWidget.setCurrentIndex(2)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key_Escape:
+            self.close()
+        if self.stackedWidget.currentIndex() == 1:
+            if int(event.modifiers()) == Qt.ControlModifier:
+                if event.key() == Qt.Key_Enter:
+                    print("db add payment and check valid")
+        elif self.stackedWidget.currentIndex() == 2:
+            if int(event.modifiers()) == Qt.ControlModifier:
+                if event.key() == Qt.Key_Enter:
+                    print("db add category and check valid")
+
--- source/main_menu.py
+++ source/main_menu.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 
 from source.db_class import Db
 from source.ui_generated_py_files.ui_mainmenu import Ui_MainMenu
+from source.create_dialog import CreateDialog
 
 
 class MainMenu(Ui_MainMenu, QMainWindow):
@@ -15,6 +16,7 @@ class MainMenu(Ui_MainMenu, QMainWindow):
         self.username_lbl.setText(self.db.get_username(self.user_id))
 
         self.exit_btn.clicked.connect(self.logout)
+        self.add_btn.clicked.connect(self.create_dialog_foo)
 
     def logout(self):
         self.db.set_remembered(self.user_id, False)
@@ -22,4 +24,8 @@ class MainMenu(Ui_MainMenu, QMainWindow):
         self.reg_window.password_le.setText("")
         self.reg_window.remember_btn.setChecked(False)
         self.close()
+
+    def create_dialog_foo(self):
+        self.create_dialog = CreateDialog(self.user_id, self)
+        self.create_dialog.show()
 
